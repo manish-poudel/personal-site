a5cf9c5ec590a2db038628408b35385fd8b13236
manish <mspoudel92@gmail.com>
2026-01-08 07:51:56 +0545

    feat:change route

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/personal-site', // Replace 'personal-site' with your repository name
-  assetPrefix: '/personal-site', // Ensures static assets are served correctly
+  // basePath: '/', // Replace 'personal-site' with your repository name
+  // assetPrefix: '/', // Ensures static assets are served correctly
   // basePath: '', // Replace 'personal-site' with your repository name
   // assetPrefix: '', // Ensures static assets are served correctly,
   images: {
